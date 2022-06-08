5f4f8f820242c8207520a0b797dfe61554463c20
Nguyen Minh <minhntq237@gmail.com>
2022-06-08 15:25:47 +0700

    test param

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function rotatePhone() {
     let beta = 2 * Math.PI * (event.beta / 360);
     let gamma = 2 * Math.PI * (event.gamma / 360);
 
-    phone.rotation.set(beta, gamma, alpha)
+    phone.rotation.set(beta - Math.PI/2, gamma, alpha)
 });
 }
 
